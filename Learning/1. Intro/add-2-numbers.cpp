3c402f02ef934a217f39fd09eca88b2cd6c6a2eb
Dima <108333654+CodingCossack@users.noreply.github.com>
2022-12-29 11:18:08 +0000

    Add files via upload

---
+++ Learning/1. Intro/add-2-numbers.cpp
@@ -0,0 +1,15 @@
+//program to add two numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double number1 = 9.9;
+    double number2 = 2.2;
+
+    double sum = number1 + number2;
+    cout << sum;
+    
+    return 0;
+}
