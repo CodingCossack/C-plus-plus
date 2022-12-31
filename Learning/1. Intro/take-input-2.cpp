170fddcddc08602353e4814dab1bd805f4bf9fa4
Dima <108333654+CodingCossack@users.noreply.github.com>
2022-12-31 23:05:52 +0000

    Add files via upload

---
+++ Learning/1. Intro/take-input-2.cpp
@@ -0,0 +1,16 @@
+//program to take 2 inputs
+#include <iostream>
+using namespace std;
+    
+int main() {
+    
+    int number1;
+    double number2;
+
+    cin >> number1 >> number2;
+
+    cout << number1 << endl;
+    cout << number2;
+   
+    return 0;
+}
