1d87ae6dbe7812fc2e49878e0822727aca62f9d9
Dima <108333654+CodingCossack@users.noreply.github.com>
2022-12-31 23:03:41 +0000

    Add files via upload

---
+++ Learning/1. Intro/print-variable.cpp
@@ -0,0 +1,15 @@
+//assigning value and printing variable
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int salary = 4950;
+
+    cout << "salary: ";
+
+    cout << salary;
+
+    return 0;
+}
