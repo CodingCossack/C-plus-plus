bf2a1061af95beed33bc8ad2f110665cf76e2897
Dima <108333654+CodingCossack@users.noreply.github.com>
2022-12-31 23:06:09 +0000

    Add files via upload

---
+++ Learning/1. Intro/type-coversion.cpp
@@ -0,0 +1,16 @@
+//program to show type conversion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number1 = 34;
+    double number2 = 54.9;      //since one vairable uses double data type...
+
+    double result = number1 + number2;  //...result should use double datatype
+
+    cout << result;
+
+  return 0;
+}
