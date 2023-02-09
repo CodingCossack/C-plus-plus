220cc5553b5ed29015345766deac9c764da645f6
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-02-09 22:47:28 +0000

    Add files via upload

---
+++ Learning/2. Control Flow/while-loop-simple.cpp
@@ -0,0 +1,16 @@
+//Program of simple while loop counting down numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 5;
+
+    while (i >= 1) {
+        cout << i << endl;
+        i = i - 1;
+    }
+
+    return 0;
+}
