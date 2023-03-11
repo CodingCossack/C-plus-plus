d4567f30e9d7cc61bf283ee526c7794c9a688991
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-03-11 13:14:36 +0000

    Add files via upload

---
+++ Learning/3. Funtions/basic-function.cpp
@@ -0,0 +1,17 @@
+// Create a function named print_numbers().
+
+#include <iostream>
+using namespace std;
+
+void print_numbers() {
+    cout << 5 << endl;
+    cout << 100 << endl;
+}
+
+int main() {
+
+    print_numbers();	// call print_numbers() two times
+    print_numbers();
+
+    return 0;
+}
