70a3d015ca86bf6ed00928a3199ccd2d846fc193
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-03-17 21:02:28 +0000

    Add files via upload

---
+++ Learning/7. Challenges/1. Intro/km-to-mi.cpp
@@ -0,0 +1,17 @@
+// convert kilometers to miles
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double km, mile;  
+
+    cin >> mile;
+
+    km = mile * 1.609344;
+
+    cout << km;
+
+    return 0;
+}
