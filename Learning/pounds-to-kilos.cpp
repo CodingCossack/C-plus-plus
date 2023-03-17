7f398c6f7972eb43f075c4fcec0fe568ba3ca20f
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-03-17 21:03:52 +0000

    Add files via upload

---
+++ Learning/pounds-to-kilos.cpp
@@ -0,0 +1,17 @@
+// convert pounds to kilograms
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double kg, pounds;
+
+    cin >> pounds;
+
+    kg = pounds * 0.45359;
+
+    cout << kg;
+
+    return 0;
+}
