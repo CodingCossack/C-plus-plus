5b03d0df9aeb580ee1ecf53fa50bfc85565869c4
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-03-21 14:31:22 +0000

    Add files via upload

---
+++ Learning/7. Challenges/2. Control Flow/print-3-sentences.cpp
@@ -0,0 +1,13 @@
+// Program to print a sentence 3 times using a loop.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for(int i = 0; i < 3; i++) {				// run a for loop from i = 0 to i < 3
+        cout << "C++ Programming is fun" << endl;
+    }
+
+    return 0;
+}
